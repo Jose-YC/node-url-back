05d37f4c2bacdaca9a392ebb2a399438dfefd76c
Jose-YC <juanjoseyamunaquecastillo02@email.com>
2026-03-05 15:47:26 -0500

    build(db): add migration for database schema changes

---
+++ prisma/migrations/20260305204609_add_table_group_url/migration.sql
@@ -0,0 +1,42 @@
+-- CreateTable
+CREATE TABLE "group" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "description" TEXT,
+    "user_id" INTEGER NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "group_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "url" (
+    "id" SERIAL NOT NULL,
+    "short_url" TEXT NOT NULL,
+    "original_url" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "statistic" BOOLEAN NOT NULL DEFAULT false,
+    "is_favorite" BOOLEAN NOT NULL DEFAULT false,
+    "user_id" INTEGER NOT NULL,
+    "group_id" INTEGER,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "deleted_at" TIMESTAMP(3),
+
+    CONSTRAINT "url_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "url_short_url_key" ON "url"("short_url");
+
+-- AddForeignKey
+ALTER TABLE "group" ADD CONSTRAINT "group_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "url" ADD CONSTRAINT "url_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "url" ADD CONSTRAINT "url_group_id_fkey" FOREIGN KEY ("group_id") REFERENCES "group"("id") ON DELETE CASCADE ON UPDATE CASCADE;
